8a1386b80a0b8920af99f47e985668d5b0b66a89
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-08-02 16:19:51 +0200

    more fixes to ci

--- mpi-scoring/execute.sh
+++ mpi-scoring/execute.sh
@@ -6,11 +6,13 @@ source activate $ENV_NAME
 
 # the output contains one file per process but we consider it all together
 
-cat $COBE_FILES/mpi-arg-trace-*.csv $COBE_FILES/cobe_dynamic.csv
-cat $MBI_FILES/mpi-arg-trace-*.csv $MBI_FILES/mbi_dynamic.csv
+cat $COBE_FILES/mpi-arg-trace-*.csv > $COBE_FILES/cobe_dynamic.csv
+cat $MBI_FILES/mpi-arg-trace-*.csv > $MBI_FILES/mbi_dynamic.csv
 
 # the full hpc applications dataset to score against
 tar -xzf mpi-arg-usage/output/output.csv.tar.gz $HPC_DATA_DIR
+
+cd $MPI_ARG_USAGE_BASE 
 
 # merge static and dynamic data
 python3 merge_dynamic_data.py --output $MBI_FILES/merged_mbi.csv --static $MBI_FILES/MBI-Static.csv --dynamic $MBI_FILES/mbi_dynamic.csv
