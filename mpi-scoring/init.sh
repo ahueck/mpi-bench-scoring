4c4048815f05ab50e94fbb3b384265941263dd0f
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-08-02 14:32:19 +0200

    work on CI

--- mpi-scoring/init.sh
+++ mpi-scoring/init.sh
@@ -2,13 +2,13 @@
 
 source bashrc
 
-git clone --depth 1 https://github.com/tudasc/mpi-arg-usage.git
+#git clone --depth 1 https://github.com/tudasc/mpi-arg-usage.git
 
 # install anaconda env
 
-conda env create -f mpi-arg-usage/environment.yml
+#conda env create -f mpi-arg-usage/environment.yml
+conda env create mpi_analysis
 
-#conda init
-#source ~/.bashrc
+source ~/anaconda3/etc/profile.d/conda.sh
 
 conda activate mpi_analysis
