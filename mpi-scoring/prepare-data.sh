73b210c1f06042c07c8386275138fcbcb958656d
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-08-02 14:55:20 +0200

    work on CI

--- mpi-scoring/prepare-data.sh
+++ mpi-scoring/prepare-data.sh
@@ -10,6 +10,4 @@ cat $MBI_FILES/mpi-arg-trace-*.csv $MBI_FILES/mbi_dynamic.csv
 # the full hpc applications dataset to score against
 tar -xzf mpi-arg-usage/output/output.csv.tar.gz $HPC_DATA_DIR
 
-#
 
-#TODO run execution
