68fa674089f847fbdc5b3bcd8dd5311a0860f46c
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-08-02 15:52:54 +0200

    change path

--- mpi-corrbench/collect_static_data.sh
+++ mpi-corrbench/collect_static_data.sh
@@ -13,5 +13,4 @@ echo "Code,Language,Type,URL,Comment,SHA" > src_location.csv
 echo "MPI-Corrbench,C,git,https://github.com/tudasc/MPI-Corrbench.git,,c89a836b3ea25df675bb6e904418b29ce1d4b548" >> src_location.csv
 
 
-
 python3 mpi_usage_analysis.py --output $MPI_ARG_TRACE_DIR/Cobe-Static.csv --code_locations src_location.csv --repo_path $MPI_CORRBENCH_BASE/..
